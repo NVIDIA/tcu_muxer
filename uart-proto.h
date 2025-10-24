fd210969a1af943f15128fd7758bc4dc84c08a36
Mike Garrett <mikeg@nvidia.com>
2025-10-24 13:25:48 -0500

    Apache 2.0 License and headers fixups Adjusted copyright message years to match original Did not find any 3rd party copyright attributes in original source
    Signed-off-by: Mike Garrett <mikeg@nvidia.com>

--- uart-proto.h
+++ uart-proto.h
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION &
+ * SPDX-FileCopyrightText: Copyright (c) 2013-2025 NVIDIA CORPORATION &
  * AFFILIATES. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
